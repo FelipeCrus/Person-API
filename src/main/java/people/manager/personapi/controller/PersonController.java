e625a0b9601e3ffee86daf316f95ff9a0ba238c2
Felipe <felipecrus2909@gmail.com>
2025-11-19 14:00:34 -0300

    Update constructors on Service and Controller

--- src/main/java/people/manager/personapi/controller/PersonController.java
+++ src/main/java/people/manager/personapi/controller/PersonController.java
@@ -2,6 +2,7 @@ package people.manager.personapi.controller;
 
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +15,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor_ = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
 
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
